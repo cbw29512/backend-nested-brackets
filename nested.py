4288d0dfd3960aaa319651db2aae11ed0639150d
Chris Wilson <cbw29512@outlook.com>
2020-02-21 11:29:34 -0500

    working

--- nested.py
+++ nested.py
@@ -1,50 +1,46 @@
+# Author "Chris Wilson, Sean, Janell, Koren, Kano"
+
 import sys
+
+
 def is_matched(s):
+    brac_types = {
+        "parasts": ["(*", "*)"],
+        "parens": ["(", ")"],
+        "squares": ["[", "]"],
+        "curlies": ["{", "}"],
+        "alligators": ["<", ">"]
+    }
+
     opn_bracket = []
     index = 0
-    for chars in s:
+    answer = "Yes"
+    while s:
         index += 1
-        if s[index-1] + s[index] == "(*":
-            opn_bracket.append(s[index-1] + s[index])
-        if s[index-1] + s[index] == "*)":
-            if len(opn_bracket) == 0:
-                return 'No ' + str(index)
-            if opn_bracket[-1] == "(*":
-                opn_bracket.pop()
-            else:
-                return 'No ' + str(index)
-        if chars == "(" or chars == "{" or chars == "<" or chars == "[":
-            opn_bracket.append(chars)
-        if chars == ")" or chars == "}" or chars == ">" or chars == "]":
-            if len(opn_bracket) == 0:
-                return 'No ' + str(index)
-        if chars == ")":
-            if opn_bracket[-1] == "(":
-                opn_bracket.pop()
-            else:
-                return 'No ' + str(index)
-        if chars == "}":
-            if opn_bracket[-1] == "{":
-                opn_bracket.pop()
-            else:
-                return 'No ' + str(index)
-        if chars == ">":
-            if opn_bracket[-1] == "<":
-                opn_bracket.pop()
-            else:
-                return 'No ' + str(index)
-        if chars == "]":
-            if opn_bracket[-1] == "[":
-                opn_bracket.pop()
-            else:
-                return 'No ' + str(index)
-    if len(opn_bracket) == 0:
-        return 'Yes'
-    else:
-        return 'No ' + str(index)
+        if s[:2] == "(*" or s[:2] == "*)":
+            token = s[:2]
+        else:
+            token = s[0]
+        for bracket_maybe in brac_types:
+            if token == brac_types[bracket_maybe][0]:
+                opn_bracket.append(token)
+            if token == brac_types[bracket_maybe][-1]:
+                if opn_bracket[-1] != brac_types[bracket_maybe][0]:
+                    answer = "NO " + str(index)
+                    token = s
+                else:
+                    opn_bracket.pop()
+        s = s[len(token):]
+    if len(opn_bracket) > 0:
+        answer = "NO " + str(index)
+    return answer
+
+
 def is_nested(line):
     """Validate a single input line for correct nesting"""
     pass
+
+
 def main(args):
     """Open the input file and call `is_nested()` for each line"""
     # linecount = 0
@@ -55,5 +51,7 @@ def main(args):
             # linecount += 1
     with open('output.txt', 'w') as wf:
         wf.write(answer)
+
+
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main('input.txt')
